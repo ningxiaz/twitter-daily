d3bb2cd029394ce40a3c84157ba12780f8962eee
Ningxia Zhang <thmmrs2298@gmail.com>
2014-04-14 00:37:55 -0700

    initial pages commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Twitter Daily
+
+A web app that generates newspaper layout from tweets, for given keywords and date. The newspaper layout will be printable.
+
+## Origin
+
+Twitter Daily is part of an art project with the topic of Sequences. It's used to automatically generate newspapers using tweets and specified information. 
+
+Digital media is transient. The Twitter yesterday is different from the Twitter today. People won't remember what people were talking about on Twitter when an event occured, such as that the Malaysian Airlines MH370 went missing. The event could have tremendous effects, but as the Twitter timeline keeps updating, the significance is soon forgotten. People go on focusing on other trending topics. However, a newspaper preserves that significance, and tangibility even decades after the event. People can always come back to the newspaper, and get a sense of the past.
---
+++ js/app.js
@@ -0,0 +1,188 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+// var tweets = ["448835295463874560", "448591420858073088", "448743319989125120", "448792602616557568", "448808036665405440", "448708478903656449", "448520235097018368", "448807042086862849", "448581681365004288", "448625431793528832", "448762559013715968", "448796551276933120", "448762240750329856", "448810399958597632", "448570923118235648", "448610332034871296", "448693929601273856", "448573322859909120", "448624522019610624", "448571104735809536", "448693119421517825", "448853864813703168"];
+
+$('#generate').click(function(){
+  keywords = $('#keywords').val();
+  date = $('#date').val();
+  number = $('#number').val();
+
+  if(keywords == undefined || keywords == ''){
+    keywords = 'MH370';
+  }
+
+  if(date == undefined || date == ''){
+    date = '2014-04-01';
+  }
+
+  if(number == undefined || number == ''){
+    number = 45;
+  }
+
+  $('.start').hide();
+  $('footer').hide();
+  $('.newspaper').show();
+  $('.toolbar').show();
+  $('.tips').show();
+
+  date_tokens = date.split('-');
+  year = parseInt(date_tokens[0]);
+  month = parseInt(date_tokens[1]);
+  day = parseInt(date_tokens[2]);
+
+  // for the newspaper subtitle
+  date = new Date(year, month - 1, day, 12, 0, 0);
+  date_string = date.toDateString();
+  $('.subtitle span').text(date_string);
+
+  mintime = Date.UTC(year, month - 1, day, 0, 0, 0) / 1000;
+  maxtime = Date.UTC(year, month - 1, day, 24, 0, 0) / 1000;
+  now = Date.now();
+  tweets = [];
+
+  url = "http://otter.topsy.com/search.js?q="+keywords+"&allow_lang=en&offset=0&perpage="+number+"&mintime="+mintime+"&maxtime="+maxtime+"&call_timestamp="+now+"&apikey=09C43A9B270A470B8EB8F2946A9369F3&_=1397196936083"
+
+  $.getJSON(url + "&callback=?", function(json){
+    list = json.response.list
+    for(i = 0; i < list.length; i++){
+      permalink = list[i].trackback_permalink;
+      tokens = permalink.split('/');
+      id = tokens[tokens.length - 1];
+      tweets.push(id);
+    }
+
+    console.log(tweets.length);
+
+    for(i = 0; i < tweets.length/3; i ++){
+      url = "https://api.twitter.com/1/statuses/oembed.json?id=" + tweets[i] + "&maxwidth=600";
+      $.getJSON(url + "&callback=?", function(json){
+        html = json.html;
+        $('.column1').append(html);
+      });
+    }
+
+    for(i = tweets.length/3; i < tweets.length; i ++){
+      url = "https://api.twitter.com/1/statuses/oembed.json?id=" + tweets[i] + "&maxwidth=350&hide_media=true";
+      $.getJSON(url + "&callback=?", function(json){
+        html = json.html;
+        $('.column2').append(html);
+      });
+    }
+  });
+});
+
+function filter(){
+  $('.tweet-container').hover(function(){
+    $(this).css('opacity', 0.6);
+    style = {
+      'position': 'absolute',
+      'display': 'block',
+      'opacity': 1,
+      'right': '44%',
+      'bottom': '50%',
+      'font-size': '24px',
+      'color': 'red',
+      'cursor': 'pointer',
+      'font-weight': 'bold',
+    };
+    remove_button = jQuery('<div/>', {
+      class: 'remove'
+    });
+    remove_button.css(style);
+    remove_button.text('Remove');
+    $(this).contents().find('body').append(remove_button);
+    console.log(remove_button);
+    iframe = $(this);
+    remove_button.click(function(){
+      iframe.css('display', 'none');
+    });
+  }, 
+  function(){
+    $(this).css('opacity', 1);
+    $(this).contents().find('.remove').remove();
+  });
+}
+
+$('#organize').click(function(){
+  $('.column1').children('iframe').each(function(){
+    img = $(this).contents().find('.autosized-media');
+    iframe_height = $(this).attr('height');
+    container = jQuery('<iframe/>', {
+      id: $(this).attr('id') + '-container',
+      class: 'tweet-container',
+      scrolling: 'no',
+      frameborder: '0',
+      allowtransparency: 'true',
+      title: 'Embedded Tweet',
+      width: '575',
+      height: iframe_height - 32,
+    });
+
+    if(img.length == 1){
+      container.prependTo('.column-left');
+    }
+    else{
+      container.appendTo('.column-left');
+    }
+
+    head = $(this).contents().find('head').html();
+    body = $(this).contents().find('body').html();
+    container.contents().find('head').append(head);
+    container.contents().find('body').append(body);
+  });
+
+  $('.column1').empty();  
+
+  $('.column2').children('iframe').each(function(){
+    iframe_height = $(this).attr('height');
+    container = jQuery('<iframe/>', {
+      id: $(this).attr('id') + '-container',
+      class: 'tweet-container',
+      scrolling: 'no',
+      frameborder: '0',
+      allowtransparency: 'true',
+      title: 'Embedded Tweet',
+      width: '350',
+      height: iframe_height - 32,
+    }).appendTo('.column-right');
+
+    head = $(this).contents().find('head').html();
+    body = $(this).contents().find('body').html();
+    container.contents().find('head').append(head);
+    container.contents().find('body').append(body);
+  });
+
+  $('.column2').empty();  
+  $('.tweet-container').contents().find('.follow-button').remove();
+  $('.tweet-container').contents().find('.footer').remove();
+  filter();
+});
+
+$('#ready').click(function(){
+  $('.tweet-container').off();
+  $('.tweet-container').contents().find('.remove').remove();
+  window.print();
+});
+
+$('#color').click(function(){
+  if($(this).hasClass('bw')){
+    $('.newspaper').addClass('bw');
+    $(this).removeClass('bw');
+    $(this).addClass('color');
+    $(this).text('Back to Color');
+  }
+  else if($(this).hasClass('color')){
+    $('.newspaper').removeClass('bw');
+    $(this).removeClass('color');
+    $(this).addClass('bw');
+    $(this).text('Black & White');
+  }
+});
+
+$('#gotit').click(function(){
+  $('.tips').hide();
+});
+
+$(document).foundation();
+
+
